01d64ca70df9a4b20bd7e1bafa8a72ccc721901d
kirti73 <kirtikumari.cs24@bmsce.ac.in>
2025-11-10 14:20:00 +0530

    Create 169.py

---
+++ 169.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        for i in nums:
+            if nums.count(i)>len(nums)/2:
+                return i
