30d8248054f773d6e8f3c5bddd9b59cb06c21ed4
kirti73 <kirtikumari.cs24@bmsce.ac.in>
2025-11-10 14:41:40 +0530

    Update 169.py

--- 169.py
+++ 169.py
@@ -4,12 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        map={}
-        max_num = None
-        max_count = 0
-        for i in nums:
-            map[i]=map.get(i,0)+1
-            if map[i] > max_count:
-                max_count = map[i]
-                max_num = i
-        return max_num
+        nums.sort()
+        return nums[len(nums)//2]
