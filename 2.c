4eb7ae9dd05bdabd176223cd27851af8b8769ebb
kirti73 <kirtikumari.cs24@bmsce.ac.in>
2025-11-24 01:16:41 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* dummy = malloc(sizeof(struct ListNode));
+    struct ListNode* current = dummy;
+    int carry = 0;
+    
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        carry = sum / 10;
+        int digit = sum % 10;
+       
+        current->next = malloc(sizeof(struct ListNode));
+        current->next->val = digit;
+        current = current->next;
+    }
+    current->next = NULL; 
+    return dummy->next; 
+}
